6d5f76fd6ed6527c32c8c058f667c48a1f46290b
smith750 <jksmith@kuali.co>
2016-11-21 16:21:43 -0700

    quickstart for angular

---
+++ grades-angular/app/main.ts
@@ -0,0 +1,6 @@
+/// <reference path="../node_modules/@types/node/index.d.ts" />
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+const platform = platformBrowserDynamic();
+platform.bootstrapModule(AppModule);
